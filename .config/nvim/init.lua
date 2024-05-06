2939834c2ca71d389bae4bf0b819f4d563ecbf85
raeperd <raeperd117@gmail.com>
2024-05-06 14:42:10 +0900

    feat(nvim): Add oil.nvim keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -804,6 +804,9 @@ require('lazy').setup({
         show_hidden = true,
       },
     },
+    init = function()
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 
