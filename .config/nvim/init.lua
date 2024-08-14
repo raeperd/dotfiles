43077c01551aa103858dcbf40be66a315349a788
raeperd <raeperd117@gmail.com>
2024-08-15 03:49:44 +0900

    feat(nvim): Fix oil.nvim config with more detail

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -810,16 +810,38 @@ require('lazy').setup({
 
   {
     'stevearc/oil.nvim',
-    opts = {
-      default_file_expolorer = true,
-      view_options = {
-        show_hidden = true,
-      },
-    },
+    opts = {},
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    config = function()
+      require('oil').setup {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+        },
+        float = {
+          padding = 2,
+          max_width = 90,
+          max_height = 0,
+        },
+        win_options = {
+          wrap = true,
+          winblend = 0,
+        },
+        keymaps = {
+          ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+          ['<C-x>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        },
+      }
+    end,
     init = function()
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
     end,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 
   {
