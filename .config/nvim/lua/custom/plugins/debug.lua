c8e099c17bb6b5ff1fde1820955740c552d4247a
raeperd <raeperd117@gmail.com>
2024-09-10 19:52:27 +0900

    feat(nvim): Fix debug shortcut

--- .config/nvim/lua/custom/plugins/debug.lua
+++ .config/nvim/lua/custom/plugins/debug.lua
@@ -31,8 +31,8 @@ return {
       -- Basic debugging keymaps, feel free to change to your liking!
       vim.keymap.set('n', '<F1>', dap.continue, { desc = 'Debug: Start/Continue' })
       vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-      vim.keymap.set('n', '<F4>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F3>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Debug: Step Out' })
       vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
       vim.keymap.set('n', '<leader>dB', function()
         dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
