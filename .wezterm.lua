e69a6a7c2138815391c7035f4f8560a9084b47e5
raeperd <raeperd117@gmail.com>
2024-10-31 21:53:19 +0900

    feat(tmux): Fix tmux config with no transparent bg

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,6 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
 
 config.window_padding = {
