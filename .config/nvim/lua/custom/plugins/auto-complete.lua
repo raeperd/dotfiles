8d8c91a7400740ae16ffe0dc8482de01e9133dc1
raeperd <raeperd117@gmail.com>
2025-01-13 12:19:32 +0900

    feat(nvim): Fix avante.nvim to use telescope file_selector

--- .config/nvim/lua/custom/plugins/auto-complete.lua
+++ .config/nvim/lua/custom/plugins/auto-complete.lua
@@ -100,9 +100,6 @@ return {
     main = 'ibl',
     opts = {},
   },
-
-  'github/copilot.vim',
-
   {
     'Exafunction/codeium.nvim',
     dependencies = {
@@ -131,22 +128,28 @@ return {
   {
     'yetone/avante.nvim',
     event = 'VeryLazy',
+    -- version = '*',
+    branch = 'main',
+    -- commit = '6ebba1526bccdb08e94aa386c07acae39261d0e2',
     lazy = false,
-    version = false, -- set this if you want to always pull the latest change
     opts = {
+      file_selector = {
+        provider = 'telescope',
+      },
       -- add any opts here
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
       --- The below dependencies are optional,
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
       'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
       'zbirenbaum/copilot.lua', -- for providers='copilot'
+      'ibhagwan/fzf-lua', -- optional
       {
         -- support for image pasting
         'HakonHarnes/img-clip.nvim',
